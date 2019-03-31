31ef0bd63c3430a6786c6dc4262ae2023de688db
Ted Carter <onewhocarts@gmail.com>
2019-03-31 14:22:28 -0700

    Added enemy type with horizontal move plan

---
+++ Bitforest SHMUP/Assets/Scripts/EnemyBehavior/MoveSidePlan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSidePlan : AppResourceMonoBehaviour
+{
+    public AnimationCurve Plan;
+
+    void Update()
+    {
+        var worldWidth = ScreenTopRight.x - ScreenBottomLeft.x;
+        var worldHeight = ScreenTopRight.y - ScreenBottomLeft.y;
+        var proportionDown = (ScreenTopRight.y - transform.position.y) / worldHeight;
+        var newX = ScreenBottomLeft.x + worldWidth * Plan.Evaluate(proportionDown);
+        transform.position += Vector3.right * (newX - transform.position.x);
+    }
+}
