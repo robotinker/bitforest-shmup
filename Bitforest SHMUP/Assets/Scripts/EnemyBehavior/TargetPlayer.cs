c77ddccbedb8041e3f9396dcc0f00913807cc161
David <pramslam@gmail.com>
2019-04-03 02:12:10 -0700

    Merge from Unity
    Targeting Shot
    Spread Shot

--- Bitforest SHMUP/Assets/Scripts/EnemyBehavior/TargetPlayer.cs
+++ Bitforest SHMUP/Assets/Scripts/EnemyBehavior/TargetPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TargetPlayer : MonoBehaviour
 {
+    public float speed = 3;
+
     private GameObject player;
 
     private void Awake()
@@ -11,8 +13,19 @@ public class TargetPlayer : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void Update()
+    private void Start()
     {
-        Debug.Log("Player position is: " + player.transform.position);
+        // Calculate target
+        Vector3 vectorToTarget = player.transform.position - transform.position;
+        float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) - 90;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = transform.rotation * q;
+
+        // Shoot! Pew pew
+        var rigidBody = GetComponent<Rigidbody2D>();
+        if (rigidBody != null)
+        {
+            rigidBody.velocity = -transform.up * speed;
+        }
     }
 }
